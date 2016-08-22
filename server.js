b34e9d0c6d3f94afe7c3ce5eeb956dda9a08b7d9
Anthony <antcho92@gmail.com>
2016-08-22 02:10:09 +0000

    set up mongodb

--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 var express = require ('express');
 var app = express();
+var mongo = require('mongodb').MongoClient
 var port = process.env.PORT || 8080;
 
 
@@ -7,3 +8,9 @@ var port = process.env.PORT || 8080;
 app.listen(port, function() {
     console.log('Listening on port ' + port);
 })
+
+var url = 'mongodb://localhost:27017/' + process.argv[2]
+mongo.connect(url, function(err, db) {
+  if (err) throw err
+  
+});
