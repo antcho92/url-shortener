29f5e6907e052c5fb9b81e0e95e6e6b4da96e7fa
Anthony <antcho92@gmail.com>
2016-08-22 05:21:53 +0000

    working on server.js

--- server.js
+++ server.js
@@ -1,16 +1,15 @@
 var express = require ('express');
 var app = express();
 var mongo = require('mongodb').MongoClient
+var shortid = require('shortid');
+var validUrl = require('valid-url');
 var port = process.env.PORT || 8080;
+var router = express.Router();
 
-
+router.get('/new/:url(*)', function(req, res, next) {
+    res.send(req.params.url);
+});
 
 app.listen(port, function() {
     console.log('Listening on port ' + port);
-})
-
-var url = 'mongodb://localhost:27017/' + process.argv[2]
-mongo.connect(url, function(err, db) {
-  if (err) throw err
-  
 });
