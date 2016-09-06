669ad7495c4b3bc41a48b8d4e49e2d9c60cb4c09
Anthony <antcho92@gmail.com>
2016-09-06 23:40:06 +0000

    working on adding links to db

--- routes/index.js
+++ routes/index.js
@@ -1,6 +1,6 @@
 var express = require('express');
 var router = express.Router();
-var mongodb = require('mongodb');
+var mongo = require('mongodb').MongoClient;
 var shortid = require('shortid');
 var validUrl = require('valid-url');
 
@@ -9,9 +9,28 @@ router.get('/', function(req, res, next) {
     res.render('index');
 });
 
-// GET new link
+// GET to handle new links
 router.get('/new/:url(*)', function(req, res, next) {
-    res.send(req.params.url);
+    //connect to mongodb and handle errors
+    mongo.connect('mongodb://localhost:27017/url-shortener', function(err, db) {
+      if (err) {
+        throw new Error('Database failed to connect!');
+      } else {
+        console.log('Successfully connected to MongoDB on port 27017.');
+        var collection = db.collection('links');
+        var params = req.params.url;
+        
+        var newLink = function(db, callback) {
+            var insertLink = { url: params, short: "test" };
+            collection.insert([insertLink]);
+            res.send(params);
+        };
+        newLink(db, function() {
+            db.close();
+        });
+      }
+    });
 });
+
 
 module.exports = router;
